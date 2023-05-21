e6c9a1fa2b3ed182e794769e009c31a3b03bc288
Grace1615 <wxu40@uiowa.edu>
2023-05-21 18:54:36 +0800

    added template

---
+++ pop.js
@@ -0,0 +1,299 @@
+const $e = function(sel){
+    return document.querySelector(sel);
+};
+const $ea = function(sel){
+    return document.querySelectorAll(sel);
+};
+
+let cdilid=0;
+
+let app = {
+    init: function(){
+        
+    },
+    setDate: function(form){
+        let now=new Date();
+        let nowhr=now.getHours();
+        let nowmin=now.getMinutes();
+        if(nowhr>12){
+            form.ampm.value='pm';
+            nowhr=nowhr-12;
+        }
+        form.mins.value=nowmin;
+        form.hours.value=nowhr;    
+        form.date.value=now.format("%y-%m-%d");       
+    },
+    curtime(){
+        let dt = new Date();
+        $e("#curtime").innerHTML = dt.format("%y-%m-%d %h:%min %APM");
+    },
+    remaining(dt,id){
+        let cdt = new Date().getTime();
+        let tdt = new Date(dt).getTime();
+        let bal = tdt-cdt;   
+        let remdiv = "";
+        if(bal > 0){
+            let days = Math.floor(bal / (1000 * 60 * 60 * 24));
+            let hours = Math.floor((bal % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let minutes = Math.floor((bal % (1000 * 60 * 60)) / (1000 * 60));     
+            remdiv = (days>0?days+" Days ":"") + (hours>0?hours+" Hr ":"") + (minutes+" Min") + " Left"; 
+        }  
+        id.innerHTML = remdiv;
+    }
+}
+
+window.onload = function(){    
+ 
+    const form = {
+        "message": document.getElementById("message"),
+        "type": document.getElementsByName("type")[1],
+        "submit": document.getElementById("remind"),
+        "inmin":  document.getElementById("minutes"),
+        "date": document.getElementById("date"),
+        "hours": document.getElementById("hours"),
+        "mins": document.getElementById("mins"),
+        "ampm": document.getElementById("apm"), 
+    };  
+    app.curtime();
+    setInterval(app.curtime, 1000);    
+    if(typeof(form.submit) === 'undefined' || form.submit === null){
+        return;
+    }
+    form.submit.onclick = function(e){
+        let data = {};        
+        data.title = "Task Reminder";
+        data.message = form.message.value;
+        if(data.message.trim() == ""){
+            form.message.focus();
+            return;
+        }
+        if(form.type.checked){
+
+            data.date = form.date.value;
+            if(data.date.trim() == ""){
+                form.date.focus();
+                return;
+            }
+            data.hours = Number(form.hours.value);
+            var apm = form.ampm.value.trim();
+            if(apm == "pm" && data.hours != 12){
+                data.hours += 12;
+            }else if(apm == "am" && data.hours == 12){
+                data.hours = 0;
+            }
+            var time = new Date(data.date);
+            time.setHours(data.hours);
+            time.setMinutes(Number(form.mins.value));
+            data.time = time.valueOf();
+            data.minutes = ((data.time-new Date().valueOf())/1000)/60;           
+        }else{
+            data.minutes = form.inmin.value;
+            if(data.minutes.trim()=="" || isNaN(data.minutes)){
+                form.inmin.focus();
+                return;
+            }
+            var time=new Date();
+            time.setMinutes(time.getMinutes() + Number(data.minutes));
+            data.time=time.valueOf();
+        }
+        data.id="task_rem" +data.time + new Date().valueOf();
+        chrome.runtime.sendMessage(data,function(response) {
+           console.log(response)
+           var lastError = chrome.runtime.lastError;
+            if (lastError) {
+                setTimeout(function(){
+                    store_items();
+                },1000)
+                console.log(lastError.message);
+            }
+            $ea('#menus a')[1].click();
+            form.message.value = "";
+            form.inmin.value = "";
+            app.setDate(form);
+            store_items();
+            setTimeout(function(){
+                store_items();
+            },1000)
+            
+        }); 
+    };
+    var mins="";
+    for(var $i=0;$i<=59;$i++){
+        var j=$i;
+        if($i<10){j="0"+j;}
+        mins+="<option value='"+$i+"'>"+j+"</option>"
+    }
+    var hrs="";
+    for(var $i=1;$i<=12;$i++){
+    var j=$i;
+    if($i<10){j="0"+j;}
+      hrs+="<option value='"+$i+"'>"+j+"</option>"
+    }
+    document.getElementById("mins").innerHTML=mins;
+    document.getElementById("hours").innerHTML=hrs;
+    var radios=document.getElementsByName("type");
+    for(var i = 0;i < radios.length;i++){
+        radios[i].onchange = function(){
+            if(this.value.trim()==='mins'){
+                ienable("minutes");
+                idisable("mins","hours","apm","date");
+            }else{
+                idisable("minutes");
+                ienable("mins","hours","apm","date");
+            }
+        };
+    }
+    app.setDate(form);
+    var cl=$ea('#menus a');
+    for(var i=0;i<cl.length;i++){
+        cl[i].onclick=function(){
+            for(var i=0;i<cl.length;i++){
+                cl[i].classList.remove("active");
+            }
+            this.classList.add('active');
+            var acttab=this.getAttribute('href');
+            var tabi=$ea('.tabi');
+            for(var i=0;i<tabi.length;i++){
+                tabi[i].style.display="none";
+            }
+            $e(acttab).style.display="block";
+        }; 
+    };
+    store_items();
+    init_del();   
+};
+
+function store_items(){   
+    chrome.storage.sync.get('tr_items',function(items){
+        var td="";
+        if(Object.keys(items).length>0){
+            for(var i=0;i<items.tr_items.length;i++){
+                td+="<tr>";
+                td+="<td>"+items.tr_items[i].message+"</td>";
+                td+="<td>"+new Date(items.tr_items[i].time).format("%m/%d/%y %h:%min %APM")+" (<span class='remtime' data-time='"+items.tr_items[i].time+"'></span> )</td>";
+                td+="<td><a class='btn btn-link del_tasks' data-id='"+items.tr_items[i].id+"'><i title='delete' class='glyphicon glyphicon-trash'></i></a>";
+                td+"</tr>";
+            }
+        }
+        if(td!=""){
+            var $fdel="<a class='btn btn-link del_tasks' data-id='full'><i title='delete all' class='glyphicon glyphicon-trash'></i>";
+            $e('#viewtasks table tbody').innerHTML=td;
+            $ea('#viewtasks table thead th')[2].innerHTML=$fdel;
+            init_del();
+        }else{
+            $e('#viewtasks table tbody').innerHTML="<tr><td colspan='3'>No Tasks</td></tr>";
+        }
+        $ea(".remtime").forEach(function(e,i){
+            var tm=Number(e.dataset.time);
+            if(tm){
+                setInterval(function(){
+                    app.remaining(tm,e);
+                },1000);                
+            }
+        });
+    });
+}
+function idisable(){
+    for(var i=0;i<arguments.length;i++){
+        document.getElementById(arguments[i]).setAttribute('disabled','disabled');
+    }
+    
+}
+function ienable(){
+    for(var i=0;i<arguments.length;i++){
+        document.getElementById(arguments[i]).removeAttribute('disabled');
+    }
+}
+function clear_task(id){
+    var fn=arguments.length>1?arguments[1]:"";
+    if(id.trim()!="full"){
+        chrome.storage.sync.get('tr_items',function(items){
+            if(Object.keys(items).length>0){
+                var tmp=[];
+                 for(var i=0;i<items.tr_items.length;i++){
+                    var tr=items.tr_items[i];
+                     if(tr.id.trim()!=id.trim()){
+                        tmp.push(tr);
+                     }
+                }
+                chrome.storage.sync.set({'tr_items':tmp},function(){
+                    if(fn!=""){
+                        fn();
+                    }
+                });
+            }
+        });
+    }else{
+        chrome.storage.sync.clear(function(){
+            if(fn!=""){
+                fn();
+            }
+        });
+    }
+}
+function init_del(){
+    var ids=$ea('.del_tasks');
+    for(var i=0;i<ids.length;i++){
+        ids[i].onclick=function(){
+            var did=this.getAttribute('data-id');
+            cdilid=did
+            $e('#delete_confirm').style.display="block";
+        };
+    }
+    document.getElementById('confirm_delete').onclick=function(){
+        clear_task(cdilid,function(){
+            $e('#delete_confirm').style.display="none";
+            store_items();
+            chrome.notifications.clear(cdilid);
+            
+        });        
+    };
+    document.getElementById('confirm_delete_no').onclick=function(){
+        $e('#delete_confirm').style.display="none";
+    };
+}
+
+
+function get_all(){
+    chrome.notifications.getAll(function(dat){
+        console.log(dat);       
+    });
+    chrome.alarms.getAll(function(dat){
+        console.log(dat);
+    });
+    chrome.storage.sync.get('tr_items',function(items){
+        console.log(items);
+    });
+}
+try{
+    Date.prototype.format=function(f){
+        var dt=this;
+        var formats={
+            "m":dt.getMonth()+1,
+            "d":dt.getDate(),
+            "w":dt.getDay(),
+            "y":dt.getFullYear(),
+            "h":dt.getHours(),
+            "min":dt.getMinutes(),
+            "s":dt.getSeconds(),
+            "ms":dt.getMilliseconds(),
+            "t":dt.getTime(),
+            "apm":dt.getHours() >= 12 ? 'pm' : 'am',
+            "APM":dt.getHours() >= 12 ? 'PM' : 'AM'            
+        };   
+        if(f.search(/apm/i)>-1){
+            formats['h'] = formats['h'] % 12;
+            formats['h']  = formats['h']  ? formats['h']  : 12;
+        }
+        for(var fm in formats){            
+            if(typeof formats[fm]=="number" && formats[fm]<10){
+                formats[fm]="0"+formats[fm];
+            }            
+            f=f.replace("%"+fm,formats[fm]);
+        }
+        return f;
+    };
+}catch(e){
+    console.log(e);
+}
+
---
+++ service_worker.js
@@ -0,0 +1,196 @@
+
+try{   
+    var app = {
+        version: "3.1",
+        saveItem: function(items){
+            return chrome.storage.sync.set(items);
+        },
+        getItem: function(item){
+            return chrome.storage.sync.get(item);
+        },
+        startUp: function(){
+            app.getItem('tr_items').then(function(items){   
+                if(Object.keys(items).length>0){
+                    let del_ids = []; 
+                    for(let i = 0; i < items.tr_items.length; i++){
+                        let tr = items.tr_items[i];
+                        let tm = tr.time;
+                        let time = new Date().valueOf();     
+                        if(tm > time){                            
+                            app.setAlarm(tr);
+                        }else{
+                            tr.cm = "Time Passed";                                           
+                            res = app.notify(tr).then(function(){                                                   
+                            }).catch((err)=>{console.error(err)});  
+                            del_ids.push(tr.id);                                  
+                        }
+                    }
+                    if(del_ids){
+                        app.delItems(del_ids);  
+                    }    
+                }    
+            });
+        },
+        saveReminder: function(data){    
+            return new Promise(function(res,rej){
+                app.getItem('tr_items').then(function(items){
+                    let item = Array();
+                    if(Object.keys(items).length > 0){
+                        item = items.tr_items;
+                        item.push(data);
+                    }else{
+                        item.push(data);
+                    }
+                    app.saveItem({'tr_items': item}).then(function(){
+                        app.setAlarm(data);
+                        res();
+                    });                       
+                });  
+            });    
+        },
+        notify: function(data){           
+
+            return new Promise(function(res,rej){
+                chrome.notifications.getPermissionLevel(function(pl){});
+                let options = {
+                    "type":"basic",
+                    "iconUrl":"icon.png",
+                    "title":data.title,
+                    "message":data.message,
+                    "contextMessage":data.cm,
+                    "requireInteraction":true
+                };           
+                chrome.notifications.create(data.id,options,function(did){             
+                    app.setSound();  
+                    res(did)
+                });                
+            });
+        },
+        setAlarm: function(data){
+            data.minutes=Math.ceil(data.minutes);
+            chrome.alarms.create(data.id, {when: data.time});// periodInMinutes:Number(data.minutes)           
+        },
+        findRem: function(id){
+            return new Promise(function(res,rej){
+                app.getItem("tr_items").then(function(items){
+                    if(Object.keys(items).length > 0){
+                        let tr = items.tr_items
+                        let rem = tr.filter((item)=>{
+                            if(item.id == id){
+                                return true;
+                            }
+                        });
+                        rem = rem[0] || {};
+                        if(rem){
+                            res(rem);
+                        }else{
+                            rej(rem);
+                        }
+                    }                    
+                });
+            });
+        },
+        delItem: function(id){
+            app.getItem("tr_items").then(function(items){
+                if(Object.keys(items).length>0){                    
+                    let tmp = items.tr_items.filter((data)=>{
+                        return data.id != id;                       
+                    });   
+                    app.saveItem({'tr_items': tmp});                   
+                }
+            });    
+        },
+        delItems: function(ids){
+            app.getItem("tr_items").then(function(items){
+                if(Object.keys(items).length>0){                    
+                    let tmp = items.tr_items.filter((data)=>{
+                        return !ids.includes(data.id);
+                    });                 
+                    app.saveItem({'tr_items': tmp});                   
+                }
+            });    
+        },
+        setSound: function(){
+            chrome.windows.create(
+                {
+                    height:1,
+                    width:1,
+                    focused: false,
+                    top: 1,
+                    left: 1,           
+                    url:"audio.html",
+                    type:"popup"
+                },
+                function(){
+        
+                },
+            )            
+        },
+        tmpNotify(msg = "Test"){
+            let data = {
+                "type":"progress",        
+                "title":"Test",
+                "message":msg,
+                "contextMessage":"",
+                "iconUrl":"icon.png",
+                "id":msg+new Date().valueOf()  
+            }
+            app.notify(data);
+        }
+    };
+    async function getCurrentTab() {
+        let queryOptions = { active: true, lastFocusedWindow: true };
+        // `tab` will either be a `tabs.Tab` instance or `undefined`.
+        let [tab] = await chrome.tabs.query(queryOptions);
+        console.log(tab);
+        return tab;
+      };
+      chrome.tabs.onActivated.addListener(tab =>{
+        chrome.tabs.get(tab.tabId, current_tab_info =>{
+            console.log(current_tab_info.url)
+        })
+      });
+
+ 
+
+    chrome.runtime.onInstalled.addListener(() => {        
+      
+        
+    });    
+    chrome.notifications.onClicked.addListener(function(nid){      
+        chrome.notifications.clear(nid);        
+    });
+    chrome.notifications.onClosed.addListener((nid,b,c)=>{
+        console.log(nid,b,c,"cleared");        
+    });
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {  
+      
+        app.saveReminder(request).then(function(){
+            sendResponse({msg:"ok1"});
+        });
+    });
+    chrome.alarms.onAlarm.addListener(function(alarm_data){      
+        app.findRem(alarm_data.name.trim()).then(function(data){           
+            if(data.message){
+                chrome.action.setIcon({path: 'icon_green.png'});
+                app.notify(data).then(function(){
+                    app.delItem(data.id);
+                    chrome.action.setIcon({path: 'icon.png'});
+                });         
+            }                  
+        });          
+    });
+    chrome.runtime.onStartup.addListener(() => {
+        app.startUp();        
+        
+    });
+    chrome.runtime.onSuspend.addListener(() => {
+       
+    });
+
+
+    
+
+}catch(e){
+    console.log("err",e)
+}
