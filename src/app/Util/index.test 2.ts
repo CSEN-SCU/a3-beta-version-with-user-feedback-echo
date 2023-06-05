77c2d8b6aedccf063352b9c054f36d5e5408e7cc
KaelXue <xxue2@scu.edu>
2023-06-05 22:21:36 +0800

    del: Duplicate documents

--- src/app/Util/index.test 2.ts
+++
@@ -1,8 +0,0 @@
-import { getRemindoroUrl } from './'
-
-describe('Remindoro url is constructed correctly', () => {
-  const id = 'porumai'
-  test('Url construction is fine', () => {
-    expect(getRemindoroUrl(id)).toEqual(`/remindoro-info/${id}`)
-  })
-})
--- src/content_scripts/index 2.js
+++
@@ -1,2 +0,0 @@
-//console.log('Content scripts has loaded')
-document.documentElement.innerHTML = '<h1>Please go back to work</h1>'
